81a403d397340f12ca32abc7c8f6d015767cf015
KRivera2394 <keanurivera2394@gmail.com>
2022-08-31 18:20:57 -0400

    Added Animations and SideNav bar

--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -21,15 +21,7 @@ const video = () => {
          <hr />
          
       </div>
-      <nav id="nav-wrap">
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Login</a></li>
-            <li><a className="smoothscroll" href="#about">SignUp</a></li>
-         </ul>
-      </nav>
+      
    </div>
 
         </div>
