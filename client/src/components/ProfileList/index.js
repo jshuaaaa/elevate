853b721fba377ff4d67a10814d82c14289fecb9f
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-08-31 12:02:13 -0400

    added sidenav

--- client/src/components/ProfileList/index.js
+++ client/src/components/ProfileList/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ProfileList = ({ profiles, title }) => {
-  if (!profiles.length) {
-    return <h3>No Profiles Yet</h3>;
+const ProfileList = ({ courses, title }) => {
+  if (!courses.length) {
+    return <h3>No courses Yet</h3>;
   }
 
   return (
     <div>
-      <h3 className="text-primary">{title}</h3>
-      <div className="flex-row justify-space-between my-4">
-        {profiles &&
-          profiles.map((profile) => (
-            <div key={profile._id} className="col-12 col-xl-6">
-              <div className="card mb-3">
-                <h4 className="card-header bg-dark text-light p-2 m-0">
-                  {profile.name} <br />
-                  <span className="text-white" style={{ fontSize: '1rem' }}>
-                    currently has {profile.skills ? profile.skills.length : 0}{' '}
+      <h3 className='text-primary'>{title}</h3>
+      <div className='flex-row justify-space-between my-4'>
+        {courses &&
+          courses.map((course) => (
+            <div key={course._id} className='col-12 col-xl-6'>
+              <div className='card mb-3'>
+                <h4 className='card-header bg-dark text-light p-2 m-0'>
+                  {course.name} <br />
+                  <span className='text-white' style={{ fontSize: "1rem" }}>
+                    currently has {course.lecture ? course.lecture.length : 0}{" "}
                     endorsed skill
-                    {profile.skills && profile.skills.length === 1 ? '' : 's'}
+                    {course.lecture && course.lecture.length === 1 ? "" : "s"}
                   </span>
                 </h4>
 
                 <Link
-                  className="btn btn-block btn-squared btn-light text-dark"
-                  to={`/profiles/${profile._id}`}
+                  className='btn btn-block btn-squared btn-light text-dark'
+                  to={`/courses/${course._id}`}
                 >
                   View and endorse their skills.
                 </Link>
