4f350fab8210b9ae09901c1a551461f14552ce68
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-08-31 19:18:27 -0400

    added section for browsing courses on main

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,8 @@ import { setContext } from "@apollo/client/link/context";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-import Home from "./pages/main";
+import Video from "./pages/main";
 import Search from "./pages/searchbar";
-
 
 // import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
@@ -49,18 +47,14 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      
         <div className='min-100-vh'>
-          <Header/>
-          <SideNav/>
+          <Header />
+          <SideNav />
           <div className='container'>
-          
             <Routes>
-
-
-              <Route path='/' element={<Home />} />
+              <Route path='/' element={<Video />} />
               <Route path='/login' element={<Search />} />
-               {/* <Route path='/login' element={<Login />} /> */}
+              {/* <Route path='/login' element={<Login />} /> */}
               <Route path='/signup' element={<Signup />} />
 
               {/* <Route path='/me' element={<Profile />} /> */}
--- client/src/components/Sidenav/index.js
+++ client/src/components/Sidenav/index.js
@@ -1,91 +1,86 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Auth from '../../utils/auth';
-import '../styles/sidenav.css'
-import '../styles/header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
+import "../styles/sidenav.css";
+import "../styles/header.css";
 
 import { AnimatePresence, motion, useCycle } from "framer-motion";
 
-
 const links = [
-    { name: "Course 1", to: "#", id: 1 },
-    { name: "Course 2", to: "#", id: 2 },
-    { name: "Course 3", to: "#", id: 3 },
-    { name: "Course 4", to: "#", id: 4 }
-  ];
-  
-  const itemVariants = {
-    closed: {
-      opacity: 0
+  { name: "Course 1", to: "#", id: 1 },
+  { name: "Course 2", to: "#", id: 2 },
+  { name: "Course 3", to: "#", id: 3 },
+  { name: "Course 4", to: "#", id: 4 },
+];
+
+const itemVariants = {
+  closed: {
+    opacity: 0,
+  },
+  open: { opacity: 1 },
+};
+
+const sideVariants = {
+  closed: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: -1,
     },
-    open: { opacity: 1 }
-  };
-  
-  const sideVariants = {
-    closed: {
-      transition: {
-        staggerChildren: 0.2,
-        staggerDirection: -1
-      }
+  },
+  open: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: 1,
     },
-    open: {
-      transition: {
-        staggerChildren: 0.2,
-        staggerDirection: 1
-      }
-    }
-  };
+  },
+};
 
 const SideNav = () => {
-    const logout = (event) => {
-      event.preventDefault();
-      Auth.logout();
-    };
-    const [open, cycleOpen] = useCycle(false, true);
-    return (
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  const [open, cycleOpen] = useCycle(false, true);
+  return (
+    <main className='sideNavCont'>
+      <AnimatePresence>
+        {open && (
+          <motion.aside
+            initial={{ width: 10 }}
+            animate={{
+              width: 300,
+            }}
+            exit={{
+              width: 0,
+              transition: { delay: 0.7, duration: 0.3 },
+            }}
+          >
+            <motion.div
+              className='container navList'
+              initial='closed'
+              animate='open'
+              exit='closed'
+              variants={sideVariants}
+            >
+              {links.map(({ name, to, id }) => (
+                <motion.a
+                  key={id}
+                  href={to}
+                  whileHover={{ scale: 1.1 }}
+                  variants={itemVariants}
+                >
+                  {name}
+                </motion.a>
+              ))}
+            </motion.div>
+          </motion.aside>
+        )}
+      </AnimatePresence>
+      <div className='btn-container'>
+        <button onClick={cycleOpen}>{open ? "Close" : "Open"}</button>
+      </div>
+    </main>
+  );
+};
 
-<main className='sideNavCont'>
-<AnimatePresence>
-{open && (
-  <motion.aside
-    initial={{ width: 10 }}
-    animate={{
-      width: 300
-    }}
-    exit={{
-      width: 0,
-      transition: { delay: 0.7, duration: 0.3 }
-    }}
-  >
-    <motion.div
-      className="container navList"
-      initial="closed"
-      animate="open"
-      exit="closed"
-      variants={sideVariants}
-    >
-      {links.map(({ name, to, id }) => (
-        <motion.a
-          key={id}
-          href={to}
-          whileHover={{ scale: 1.1 }}
-          variants={itemVariants}
-         
-        >
-          {name}
-        </motion.a>
-      ))}
-    </motion.div>
-  </motion.aside>
-)}
-</AnimatePresence>
-<div className="btn-container">
-<button onClick={cycleOpen}>{open ? "Close" : "Open"}</button>
-</div>
-</main>
-    );
-      }
-
-  
-
-      export default SideNav;
+export default SideNav;
