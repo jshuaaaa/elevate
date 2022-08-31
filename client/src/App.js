2f0b0821980e099ebd299d4a5e62e373f4be38db
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-08-31 14:43:57 -0400

    commented out code in login so that the import doesn't break

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,12 @@ import { setContext } from "@apollo/client/link/context";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 import Home from "./pages/main";
 import Search from "./pages/searchbar";
 
-
 // import Profile from "./pages/Profile";
 // import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 // import Course from "./pages/Course";
 // import main from "./pages/main"
 // import searchbar from "./pages/searchbar"
@@ -52,9 +50,8 @@ function App() {
           <Header />
           <div className='container'>
             <Routes>
-           
               <Route path='/' element={<Home />} />
-              <Route path='/login' element={<Search />} />
+              <Route path='/login' element={<Login />} />
               {/* <Route path='/signup' element={<Signup />} /> */}
               {/* <Route path='/me' element={<Profile />} /> */}
               {/* <Route path='/courses/:courseId' element={<Course/>}/> */}
