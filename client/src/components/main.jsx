6c7dcbb1082132734f88c55364d1439de03eae65
alxsaunders <alexsaunders242@gmail.com>
2022-09-02 10:47:55 -0400

    course section

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import videoBg from '../assets/education.mp4'
 import '../components/styles/main.css'
-import { useQuery } from '@apollo/client';
-import CourseList from '../components/CourseList'
-import { QUERY_COURSES } from '../utils/queries'
+
 
 import searchicon from '../assets/searchicon.png'
 
@@ -12,8 +10,7 @@ import SearchBar from './SearchBar';
 
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_COURSES);
-  const courses = data?.courses || [];
+ 
   return (
     <div className='main flex' id='home'>
       
@@ -35,15 +32,7 @@ const Home = () => {
    </div>
    
         </div>
-        <div className="courseMain">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <CourseList
-            courses={courses}
-            title="Featured Courses" />
-        )}
-            </div>
+    
        
     </div>
     
