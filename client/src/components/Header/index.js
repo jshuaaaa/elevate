2991cdea9d3bfd51d10a85c53cd4068477294b28
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-09-02 11:33:46 -0400

    fixed some login logic for header and sidenav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,166 +4,89 @@ import "../styles/header.css";
 import Auth from "../../utils/auth";
 import { motion } from "framer-motion";
 
-const logout = (event) => {
-  event.preventDefault();
-  Auth.logout();
-};
-
-
-class Header extends Component {
-  
-  constructor(props) {
-    super(props)
-    
-this.state = {
-  isLoggedIn: false
-}
-
-}
-
-render(){
-  if (this.state.isLoggedIn) {
-    return (
-     
-      <header className='text-dark '>
+const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <header className='bg-info text-dark mb-4 py-3  '>
       <div className='headerC d-flex'>
-        <Link className='text-dark' to='/'></Link>
-        <div className='welcome'>Welcome Back, User!</div>
         <div
           className='logAndSign d-flex align-self-end align-items-end
         '
         >
           {Auth.loggedIn() ? (
             <>
-              <Link className='btn btn-lg btn-info m-2' to='/me'>
-                {Auth.getProfile().data.name}'s profile
-              </Link>
-              <Link to='/courses/add'>
-                <motion.a
-                  initial={{ x: -1400 }}
+              <div>
+                <h2>Welcome back, {Auth.getProfile().data.name}!</h2>
+              </div>
+              <div>
+                <Link to='/courses/add'>
+                  <motion.a
+                    initial={{ x: -1400 }}
+                    animate={{ x: 0 }}
+                    transition={{ delay: 1.0, type: "tween" }}
+                    className='btn btn-lg btn-primary m-2'
+                  >
+                    Create course
+                  </motion.a>
+                </Link>
+                <Link to='/me'>
+                  <motion.a
+                    initial={{ x: -1400 }}
+                    animate={{ x: 0 }}
+                    transition={{ delay: 1.0, type: "tween" }}
+                    className='btn btn-lg btn-primary m-2'
+                  >
+                    View My Profile
+                  </motion.a>
+                </Link>
+                <motion.button
+                  initial={{ x: 1400 }}
                   animate={{ x: 0 }}
                   transition={{ delay: 1.0, type: "tween" }}
-                  className='btn btn-lg btn-primary m-2'
+                  className='btn btn-lg btn-light m-2'
+                  onClick={logout}
                 >
-                  Create course
-                </motion.a>
-              </Link>
-              <Link to='/me'>
-                <motion.a
-                  initial={{ x: -1400 }}
-                  animate={{ x: 0 }}
-                  transition={{ delay: 1.0, type: "tween", duration: 0.5 }}
-                  className='btn btn-lg btn-primary m-2'
-                >
-                  View My Profile
-                </motion.a>
-              </Link>
-              <motion.button
-                initial={{ x: 1400 }}
-                animate={{ x: 0 }}
-                transition={{ delay: 0.5, type: "tween", duration: 0.5 }}
-                className='btn btn-lg btn-light m-2'
-                onClick={logout}
-              >
-                Logout
-              </motion.button>
+                  Logout
+                </motion.button>
+              </div>
             </>
           ) : (
             <>
-              <Link to='/login'>
-                <motion.a
-                  initial={{ x: 1400 }}
-                  animate={{ x: 0 }}
-                  transition={{ delay: 0.1, type: "tween", duration: 0.5 }}
-                  className='logAndSign align-self-end btn btn-lg btn-light m-2'
-                >
-                  Login
-                </motion.a>
-              </Link>
+              <div>
+                <h2>Welcome Guest!</h2>
+              </div>
+              <div>
+                <Link to='/login'>
+                  <motion.a
+                    initial={{ x: 1400 }}
+                    animate={{ x: 0 }}
+                    transition={{ delay: 0.9, type: "tween" }}
+                    className='logAndSign align-self-end btn btn-lg btn-primary m-2'
+                  >
+                    Login
+                  </motion.a>
+                </Link>
 
-              <Link to='/signup'>
-                <motion.a
-                  initial={{ x: 1400 }}
-                  animate={{ x: 0 }}
-                  transition={{ delay: 0.2, type: "tween", duration: 0.5 }}
-                  className='logAndSign align-self-end btn btn-lg btn-light m-2'
-                >
-                  Signup
-                </motion.a>
-              </Link>
+                <Link to='/signup'>
+                  <motion.a
+                    initial={{ x: 1400 }}
+                    animate={{ x: 0 }}
+                    transition={{ delay: 1.0, type: "tween" }}
+                    className='logAndSign align-self-end btn btn-lg btn-light m-2'
+                  >
+                    Signup
+                  </motion.a>
+                </Link>
+              </div>
             </>
           )}
         </div>
       </div>
     </header>
   );
-}
-else {
-    return (
-      <header className='text-dark '>
-      <div className='headerC d-flex'>
-        <Link className='text-dark' to='/'></Link>
-        <div className='welcome'>Welcome Guest!</div>
-        <div
-          className='logAndSign d-flex align-self-end align-items-end
-        '
-        >
-          {Auth.loggedIn() ? (
-            <>
-              <Link to='/me'>
-                <motion.a
-                  initial={{ x: -1400 }}
-                  animate={{ x: 0 }}
-                  transition={{ delay: 1.0, type: "tween" ,duration: 0.5 }}
-                  className='btn btn-lg btn-primary m-2'
-                >
-                  View My Profile
-                </motion.a>
-              </Link>
-              <motion.button
-                initial={{ x: 1400 }}
-                animate={{ x: 0 }}
-                transition={{ delay: 0.5, type: "tween", duration: 0.5 }}
-                className='btn btn-lg btn-light m-2'
-                onClick={logout}
-              >
-                Logout
-              </motion.button>
-            </>
-          ) : (
-            <>
-              <Link to='/login'>
-                <motion.a
-                  initial={{ x: 1400 }}
-                  animate={{ x: 0 }}
-                  transition={{ delay: 0.1, type: "tween", duration: 0.5 }}
-                  className='logAndSign align-self-end btn btn-lg btn-light m-2'
-                >
-                  Login
-                </motion.a>
-              </Link>
-
-              <Link to='/signup'>
-                <motion.a
-                  initial={{ x: 1400 }}
-                  animate={{ x: 0 }}
-                  transition={{ delay: 0.2, type: "tween", duration: 0.5 }}
-                  className='logAndSign align-self-end btn btn-lg btn-light m-2'
-                >
-                  Signup
-                </motion.a>
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    </header>
-  );
-}
 };
 
-    
-  }
-
-    
 export default Header;
