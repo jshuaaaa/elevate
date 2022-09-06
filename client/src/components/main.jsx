f9495f6acc514f34cbc579bd865f89bb9c7e4058
Alex <104272847+alxsaunders@users.noreply.github.com>
2022-09-06 07:19:01 -0400

    Revert "Kmod"

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -4,15 +4,11 @@ import '../components/styles/main.css'
 import CourseSection from '../components/CourseSection'
 import SearchBar from './SearchBar';
 
-
 const Home = () => {
- 
  
   return (
     <div className='main flex' id='home'>
-       
         <video className='video2' src={videoBg} autoPlay loop muted />
-        
 
         <div className=" sizeM">
         <div className="row banner sBox">
