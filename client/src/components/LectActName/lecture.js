15c11156e8803c9d162c79b569e0b26651ea666a
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-09-06 15:07:01 -0400

    fixed issue with frontend routing

--- client/src/components/LectActName/lecture.js
+++ client/src/components/LectActName/lecture.js
@@ -17,7 +17,7 @@ const LectureName = (props) => {
 
   return (
     <>
-      <h4 {...props} className='card-header bg-info p-1'>
+      <h4 className='card-header bg-info p-1'>
         {lecture.name}
       </h4>
       <iframe src={lecture.url}></iframe>
