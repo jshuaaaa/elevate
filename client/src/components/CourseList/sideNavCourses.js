2d0790311146286acd3b9a4ebe0b9d68c99d0b3a
rheangocle <rheangocle@gmail.com>
2022-09-06 20:56:23 -0400

    courses on sidenav are courses created by user not all courses

--- client/src/components/CourseList/sideNavCourses.js
+++ client/src/components/CourseList/sideNavCourses.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_COURSES } from "../../utils/queries";
+import { QUERY_SINGLE_PROFILE, QUERY_ME } from "../../utils/queries";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
+import { Navigate, useParams } from "react-router-dom";
 
 const itemVariants = {
   closed: {
@@ -12,8 +14,16 @@ const itemVariants = {
 };
 
 const SideNavCourses = () => {
-  const { loading, data } = useQuery(QUERY_COURSES);
-  const courses = data?.courses || [];
+  // const { loading, data } = useQuery(QUERY_COURSES);
+  // const courses = data?.courses || [];
+  const { profileId } = useParams();
+  const { loading, data } = useQuery(
+    profileId ? QUERY_SINGLE_PROFILE : QUERY_ME,
+    {
+      variables: { profileId: profileId },
+    }
+  );
+  const profile = data?.me || data?.profile || {};
 
   return (
     <>
@@ -21,15 +31,19 @@ const SideNavCourses = () => {
         <div>Loading...</div>
       ) : (
         <>
-          {courses.map((course) => (
-            <motion.a
-              key={course.name}
-              whileHover={{ scale: 1.1 }}
-              variants={itemVariants}
-            >
-              <Link to={`/courses/${course._id}`}>{course.name}</Link>
-            </motion.a>
-          ))}
+          {profile.courses?.length > 0 && (
+            <>
+              {profile.courses.map((course) => (
+                <motion.a
+                  key={course.name}
+                  whileHover={{ scale: 1.1 }}
+                  variants={itemVariants}
+                >
+                  <Link to={`/courses/${course._id}`}>{course.name}</Link>
+                </motion.a>
+              ))}
+            </>
+          )}
         </>
       )}
     </>
